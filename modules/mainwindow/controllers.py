ee72962044e40cd28efde52cc96c9a678aa804fa
Ashfaque Alam <67138411+ashfaque@users.noreply.github.com>
2023-10-20 11:13:42 +0530

    updated

--- modules/mainwindow/controllers.py
+++ modules/mainwindow/controllers.py
@@ -1,7 +1,19 @@
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.init_ui()
+# class MainWindow(QMainWindow):
+#     def __init__(self):
+#         super().__init__()
+#         self.init_ui()
 
-    def init_ui(self):
-        # Set up your main window's layout, menus, and actions
+#     def init_ui(self):
+#         # Set up your main window's layout, menus, and actions
+
+
+class YourController:
+    def __init__(self, view):
+        self.view = view
+
+        # Connect signals from the view to slots in the controller
+        self.view.some_button.clicked.connect(self.some_action)
+
+    def some_action(self):
+        # Perform actions, such as API calls, switching screens, etc.
+        pass
