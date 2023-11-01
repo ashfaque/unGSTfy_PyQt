ef649fa4070589c345cb9435aa7fa1f96fb9dabd
Ashfaque Alam <67138411+ashfaque@users.noreply.github.com>
2023-11-01 14:13:01 +0530

    updated

--- commands.sh
+++ commands.sh
@@ -10,7 +10,7 @@ export gitpush=$(echo -e "git add --all && git commit -m 'updated' && git pull o
 export designer=$(echo -e 'C:/Users/${USERNAME}/.conda/envs/gstenv/Lib/site-packages/qt6_applications/Qt/bin/designer.exe &')
 export ui2py=$(echo -e 'pyuic6 -x _ui_archives/untitled.ui -o _ui_archives/untitled.py')    # * Just save .ui file in ui/ dir of this project and run this command in the terminal.
 export py2exe=$(echo -e 'pyinstaller --name=unGSTfy --noconfirm --onefile --windowed --icon=assets/icons/gst_logo.ico main.py')
-
+export buildclean=$(echo -e 'rm -rf unGSTfy.spec build dist')
 
 #######################################################################################################
 
