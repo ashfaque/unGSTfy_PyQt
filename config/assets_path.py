9d60d26c46f2e659fc726c94833090bde340aebc
Ashfaque Alam <67138411+ashfaque@users.noreply.github.com>
2023-11-02 22:41:52 +0530

    updated

--- config/assets_path.py
+++ config/assets_path.py
@@ -7,12 +7,12 @@ from utils.global_functions import assets_base_path_according_to_dev_or_exec
 APP_LOGO_PATH = assets_base_path_according_to_dev_or_exec('assets/images/gst_logo.png')
 
 FONTS = {
-    'TF2_BUILD_PATH': assets_base_path_according_to_dev_or_exec('assets/fonts/TF2Build.ttf')
-    , 'TF2_SECONDARY_PATH': assets_base_path_according_to_dev_or_exec('assets/fonts/TF2Secondary.ttf')
+    'TF2 Build': assets_base_path_according_to_dev_or_exec('assets/fonts/TF2Build.ttf')
+    , 'TF2 Secondary': assets_base_path_according_to_dev_or_exec('assets/fonts/TF2Secondary.ttf')
 }
 
 
 
 def initialize_assets():
     for each_font_key, each_font_value in FONTS.items():
-        QtGui.QFontDwatabase.addApplicationFont(each_font_value)
+        QtGui.QFontDatabase.addApplicationFont(each_font_value)
