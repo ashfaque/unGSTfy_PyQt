2f6224223f2b93aeb6cfe9e471b2d35318e66f6e
Ashfaque Alam <67138411+ashfaque@users.noreply.github.com>
2023-11-03 11:21:41 +0530

    removed Font.md file

--- commands.sh
+++ commands.sh
@@ -9,10 +9,10 @@ source "$PWD/aliases.sh"    # Write: USERNAME="USERNAME_HERE"
 export gitpush=$(echo -e "git add --all && git commit -m 'updated' && git pull origin HEAD && git push origin HEAD")
 export designer=$(echo -e 'C:/Users/${USERNAME}/.conda/envs/gstenv/Lib/site-packages/qt6_applications/Qt/bin/designer.exe &')
 export ui2py=$(echo -e 'pyuic6 -x _ui_archives/untitled.ui -o _ui_archives/untitled.py')    # * Just save .ui file in ui/ dir of this project and run this command in the terminal.
-# export py2exe=$(echo -e 'pyinstaller --name=unGSTfy --noconfirm --onefile --windowed --icon=assets/icons/gst_logo.ico main.py')
 export py2exe=$(echo -e 'pyinstaller --noconfirm --onefile --windowed --clean --name "unGSTfy" --icon "assets/icons/gst_logo.ico" --log-level "INFO" --add-data "assets;assets/" main.py')
-export debugbuild=$(echo -e 'pyinstaller --noconfirm --onedir --console --clean --name "unGSTfy" --icon "assets/icons/gst_logo.ico" --log-level "INFO" --add-data "assets;assets/"  main.py')
-export buildclean=$(echo -e 'rm -rf unGSTfy.spec build dist')
+export py2direxe=$(echo -e 'pyinstaller --noconfirm --onedir --windowed --clean --name "unGSTfy" --icon "assets/icons/gst_logo.ico" --log-level "INFO" --add-data "assets;assets/" main.py')
+export py2debugexe=$(echo -e 'pyinstaller --noconfirm --onedir --console --clean --name "unGSTfy" --icon "assets/icons/gst_logo.ico" --log-level "INFO" --add-data "assets;assets/"  main.py')
+export clean=$(echo -e 'rm -rf unGSTfy.spec build dist')
 
 #######################################################################################################
 
