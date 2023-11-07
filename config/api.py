ab3f2ca0de1653b5eacdfebcefb47a9cb62775a4
Ashfaque Alam <67138411+ashfaque@users.noreply.github.com>
2023-11-08 00:05:04 +0530

    updated

--- config/api.py
+++ config/api.py
@@ -1,5 +1,8 @@
+import urllib.request as urllib_request
+
 BASE_URL = 'http://api.exchangeratesapi.io/'
-ACCESS_KEY = '012432093fd9fd5a4f33393fee22c95b'    # TODO: Change API key later at somewhere outside this application.
+API_KEY_URL = 'https://pastebin.com/raw/euRCxULz'
+ACCESS_KEY = urllib_request.urlopen(API_KEY_URL).read().decode('utf-8')
 
 API_CURRENCIES_LIST = {
     'url': BASE_URL+'v1/symbols?'
