d5003a29a10d51d09d1251eecc791e80a1740b26
Ashfaque Alam <67138411+ashfaque@users.noreply.github.com>
2023-11-22 23:50:18 +0530

    added launching only single instance of the application feature

--- README.md
+++ README.md
@@ -32,3 +32,14 @@ pyinstaller --noconfirm --onedir --console --clean --name "unGSTfy" --icon "D:/F
 - Windows : `C:/Users/USERNAME/AppData/Roaming/unGSTfy/`
 - macOS : 
 - LINUX : 
+
+
+#### Use QThread for long running task without freezing GUI: https://realpython.com/python-pyqt-qthread/
+
+
+'''
+os.path.expanduser
+Used to expand an initial path component ~( tilde symbol) or ~user in the given path to user's home directory.
+On Unix platforms, an initial ~ is replaced by the value of HOME environment variable, if it is set. Otherwise, os.path.expanduser() method search for user’s home directory in password directory using an in-built module pwd. Path containing an initial ~user component is looked up directly in the password directory.
+On Windows platform, an initial ~ is replaced by the value of HOME and USERPROFILE environment variable, if it is set. Otherwise, HOMEPATH and HOMEDRIVE environment variable will be used. While Path containing an initial ~user component is handled by replacing the last directory component with ~user from the path derived above.
+'''
--- main.py
+++ main.py
@@ -1,16 +1,42 @@
 import atexit
+from pdb import set_trace as bp
 import sys
 
 from PyQt6.QtWidgets import QApplication
+from PyQt6.QtCore import QFile, QLockFile
 
 from modules.mainwindow.mainwindow_controllers import MainWindowController
 from config.settings import LocalDatabaseManager
-from config import app_logging    # Setup logging for the entire application.
+from config.ui_element_names import APP_NAME
+from config import app_logging    # ? Setup logging for the entire application.
 
+
+
+def remove_lock_file():
+    if lock_file.isLocked():    # As lock_file is defined in global scope, it can be accessed here.
+        lock_file.unlock()    # Removes the lock file, when the application is about to close.
+    QFile(lock_file_path).remove()    # * If execution reaches here, it auto removes that lock file if any dangling lock files are present in the file system, which is unremoved after the application closed. Due to user killing the process or tampering with the lock file, etc.
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    main_window_controller_obj = MainWindowController()
-    atexit.register(LocalDatabaseManager.close_connection)
-    sys.exit(app.exec())
+
+    lock_file_path = f"{app_logging.get_app_data_dir()}/.{APP_NAME}.lock"
+    lock_file = QLockFile(QFile(lock_file_path).fileName())
+
+    if lock_file.tryLock():    # If able to lock the file, it means no other instance of the application is running.
+        main_window_controller_obj = MainWindowController()
+        atexit.register(LocalDatabaseManager.close_connection)
+        atexit.register(remove_lock_file)
+        sys.exit(app.exec())
+    else:
+        # * If user is facing any problem launching the application, like it crashes when launched, then it might be due to the dangling lock file present in the file system. So, reboot the system & remove the lock file and try again.
+        remove_lock_file()
+        # from PyQt6.QtWidgets import QMessageBox
+        # error_message = QMessageBox()
+        # error_message.setIcon(QMessageBox.Warning)
+        # error_message.setWindowTitle("Error")
+        # error_message.setText("The application is already running!")
+        # error_message.setStandardButtons(QMessageBox.Ok)
+        # error_message.exec()
+        app_logging.trigger_manual_exception_and_log_it("The application is already running!", close_app=True)
