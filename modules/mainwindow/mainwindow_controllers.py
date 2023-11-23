bde5dc555e27a35845460aa4d53eec29aaef6cc2
Ashfaque Alam <67138411+ashfaque@users.noreply.github.com>
2023-11-23 23:51:39 +0530

    added feature to run the application in the background even if the application is closed using the system close buttons, the application will only close if closed by the application Exit buttons in the Tray Icon context menu button or from the application menu buttons

--- modules/mainwindow/mainwindow_controllers.py
+++ modules/mainwindow/mainwindow_controllers.py
@@ -46,6 +46,30 @@ class MainWindowController(MainWindowView):
         self.aboutView.exec()
 
     def closeEvent(self, event):
+        if event.spontaneous():  # ? If event is NOT triggered by the system (like clicking the close button)
+            self.hide()    # * Hide the GUI.
+            event.ignore()    # Ignore the event.
+        else:    # If triggered by the user (like clicking the `Exit` option in the menu bar of the application or system tray context menu).
+            reply = QtWidgets.QMessageBox.question(
+                                            self,
+                                            "Exit",
+                                            "Are you sure you want to exit?",
+                                            QtWidgets.QMessageBox.StandardButton.Yes | QtWidgets.QMessageBox.StandardButton.No,
+                                            QtWidgets.QMessageBox.StandardButton.No,
+                                        )
+            if reply == QtWidgets.QMessageBox.StandardButton.Yes:
+                event.accept()    # Accept the event. This will close the application.
+            else:
+                event.ignore()    # Ignore the event.
+
+    '''
+    # ? Used for closing the application when the user clicks on the close button of the window title bar.
+    # ? Or, when the user presses Alt+F4, or when the user presses Alt+Space and then clicks on the Close option in the system menu.
+    # ? Or, when the user presses the close button of the taskbar icon.
+    # ? Or, when the user presses the close button of the taskbar thumbnail preview.
+    # ? Or, when the user presses the close button of the system tray icon.
+    # ? Or, when user presses the Exit option in the menu bar of the application.
+    def closeEvent(self, event):
         reply = QtWidgets.QMessageBox.question(
                                         self,
                                         "Exit",
@@ -57,6 +81,7 @@ class MainWindowController(MainWindowView):
             event.accept()
         else:
             event.ignore()
+    '''
 
     '''
     # To change the style of the exit dialog.
